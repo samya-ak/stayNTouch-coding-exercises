2cc3ffd2d8cf24b76344f315539e2321dae977fe
samya-ak <samyakmaharjan@lftechnology.com>
2025-04-14 17:54:39 +0545

    Solve HTML Scraper

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # A sample Gemfile
 source 'https://rubygems.org'
 
+gem 'nokogiri', '~> 1.18', '>= 1.18.7'
 gem 'rspec', '~> 3.8'
--- code/puppies.rb
+++ code/puppies.rb
@@ -1,13 +1,39 @@
 #!/usr/bin/env ruby
 
-# Build a scraper for a dog adoption website. This scraper should open the paws.html page and collect all baby female dogs on each page. The scraper
-# should return the href sources for each matching dog. The DIRECTORY constant contains directory containing paws.html.
-#
-# Hint: You will likely want to use an HTML parser like Nokogiri and use the page's pagination to iterate each page.
+require 'nokogiri'
+
+# Builds a scraper for a dog adoption website. This scraper opens the paws.html page and collects all baby female dogs on each page. The scraper
+# returns the href sources for each matching dog. The DIRECTORY constant contains directory containing paws.html.
 #
 class Puppies
   DIRECTORY = ::File.join(::File.dirname(__FILE__), '../data')
+  INDEX_FILE = File.join(DIRECTORY, 'paws.html')
 
+  # From pagination, extract all the pages
+  def self.extract_page_paths
+    index = Nokogiri::HTML(File.read(INDEX_FILE))
+    index.css('nav.pagination a.page-link')
+         .map { |a| a['href'] }
+         .uniq
+         .compact
+         .filter { |path| path != '#' }
+  end
+
+  # Iterate through extracted pages and get all 'Female - Baby' dogs
   def self.parse
+    page_paths = extract_page_paths
+    results = []
+
+    page_paths.each do |path|
+      page_path = File.join(DIRECTORY, path)
+      next unless File.exist?(page_path)
+
+      page = Nokogiri::HTML(File.read(page_path))
+      page.css('div.col-sm-4.dog a').each do |a|
+        results.push(a['href']) if a.text.include?('Female - Baby')
+      end
+    end
+
+    results
   end
 end
